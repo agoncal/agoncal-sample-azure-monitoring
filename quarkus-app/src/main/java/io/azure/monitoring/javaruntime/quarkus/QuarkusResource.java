864a0ffe8ebe5835143a9af5551a7022c7645f98
Antonio Goncalves <antonio.goncalves@gmail.com>
2025-02-25 17:24:12 +0100

    Quarkus

--- quarkus-app/src/main/java/io/azure/monitoring/javaruntime/quarkus/QuarkusResource.java
+++ quarkus-app/src/main/java/io/azure/monitoring/javaruntime/quarkus/QuarkusResource.java
@@ -1,16 +1,107 @@
 package io.azure.monitoring.javaruntime.quarkus;
 
+import io.azure.monitoring.javaruntime.commons.AIUtil;
+import static io.azure.monitoring.javaruntime.commons.JavaRuntime.QUARKUS;
+import io.azure.monitoring.javaruntime.commons.LoadUtil;
+import io.azure.monitoring.javaruntime.commons.Statistics;
+import jakarta.ws.rs.DefaultValue;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
+import static java.lang.System.Logger.Level.INFO;
+
+import java.lang.System.Logger;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.List;
 
 @Path("/quarkus")
+@Produces(MediaType.TEXT_PLAIN)
 public class QuarkusResource {
 
+    private static final Logger LOGGER = System.getLogger(QuarkusResource.class.getName());
+
+    private final StatisticsRepository repository;
+
+    public QuarkusResource(StatisticsRepository statisticsRepository) {
+        this.repository = statisticsRepository;
+    }
+
+    /**
+     * Says hello.
+     * {@code curl 'localhost:8701/quarkus'}
+     *
+     * @return hello
+     */
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello RESTEasy";
+        LOGGER.log(INFO, "Quarkus: hello");
+        return "Quarkus: hello";
+    }
+
+    /**
+     * Simulates requests that use a lot of CPU.
+     * {@code curl 'localhost:8701/quarkus/load'}
+     * {@code curl 'localhost:8701/quarkus/load?cpu=10'}
+     * {@code curl 'localhost:8701/quarkus/load?cpu=10&memory=20&db=true&llm=true&desc=Native'}
+     *
+     * @param iterationForCpu the number of iterations to run (times 20,000).
+     * @param bitesForMemory  the number of megabytes to eat
+     * @return the result
+     */
+    @GET
+    @Path("/load")
+    public String cpu(@QueryParam("cpu") @DefaultValue("1") Long iterationForCpu,
+                      @QueryParam("memory") @DefaultValue("1") Integer bitesForMemory,
+                      @QueryParam("db") @DefaultValue("false") Boolean db,
+                      @QueryParam("llm") @DefaultValue("false") Boolean llm,
+                      @QueryParam("desc") @DefaultValue("JVM") String desc) {
+        String msg = "Quarkus: load: %d Cpu Iterations - %d Memory Bites - DB %b - LLM %b - %s.".formatted(iterationForCpu, bitesForMemory, db, llm, desc);
+        LOGGER.log(INFO, msg);
+        Instant start = Instant.now();
+
+        // Consume CPU
+        LoadUtil.loadCPU(iterationForCpu);
+
+        // Consume Memory
+        LoadUtil.loadMemory(bitesForMemory);
+
+        // Invoke LLM
+        if (llm) {
+            String answer = AIUtil.askForMonitoringHelp(iterationForCpu, bitesForMemory, start);
+            msg += " The prompt has been received from the LLM:" + answer;
+        }
+
+        // Invoke DB
+        if (db) {
+            Statistics statistics = new Statistics();
+            statistics.iterationForCpu = iterationForCpu;
+            statistics.bitesForMemory = bitesForMemory;
+            statistics.duration = Duration.between(start, Instant.now());
+            statistics.javaRuntime = QUARKUS;
+            statistics.description = msg;
+            repository.persist(statistics);
+            msg += " The result is persisted in the database.";
+        }
+
+        // Return statistics result
+        msg += " All that in " + Duration.between(start, Instant.now()).getNano() + " nano-seconds.";
+        return msg;
+    }
+
+    /**
+     * Returns what's in the database.
+     * {@code curl 'localhost:8701/quarkus/stats'}
+     *
+     * @return the list of Statistics.
+     */
+    @GET
+    @Path("/stats")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Statistics> stats() {
+        LOGGER.log(INFO, "Quarkus: retrieving statistics");
+        return repository.findAll().list();
     }
 }
--- quarkus-app/src/test/java/io/azure/monitoring/javaruntime/quarkus/QuarkusResourceTest.java
+++ quarkus-app/src/test/java/io/azure/monitoring/javaruntime/quarkus/QuarkusResourceTest.java
@@ -1,20 +1,72 @@
 package io.azure.monitoring.javaruntime.quarkus;
 
 import io.quarkus.test.junit.QuarkusTest;
-import org.junit.jupiter.api.Test;
-
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.endsWith;
+import static org.hamcrest.Matchers.startsWith;
+import org.junit.jupiter.api.Test;
 
 @QuarkusTest
 class QuarkusResourceTest {
     @Test
     void testHelloEndpoint() {
         given()
-          .when().get("/quarkus")
-          .then()
-             .statusCode(200)
-             .body(is("Hello RESTEasy"));
+            .when().get("/quarkus")
+            .then()
+            .statusCode(200)
+            .body(is("Quarkus: hello"));
     }
 
+    @Test
+    void testLoadEndpointWithDefault() {
+        given()
+            .when().get("/quarkus/load")
+            .then()
+            .statusCode(200)
+            .body(startsWith("Quarkus: load: 1 Cpu Iterations - 1 Memory Bites - DB false - LLM false"))
+            .body(endsWith("nano-seconds."));
+    }
+
+    @Test
+    void testLoadEndpointOverrideCPUandMemory() {
+        given().param("cpu", 10).param("memory", 20)
+            .when().get("/quarkus/load")
+            .then()
+            .statusCode(200)
+            .body(startsWith("Quarkus: load: 10 Cpu Iterations - 20 Memory Bites - DB false - LLM false"))
+            .body(endsWith("nano-seconds."));
+    }
+
+    @Test
+    void testLoadEndpointWithDB() {
+        given().param("db", true)
+            .when().get("/quarkus/load")
+            .then()
+            .statusCode(200)
+            .body(startsWith("Quarkus: load: 1 Cpu Iterations - 1 Memory Bites - DB true - LLM false"))
+            .body(containsString("The result is persisted in the database."))
+            .body(endsWith("nano-seconds."));
+    }
+
+
+    @Test
+    void testLoadEndpointWithLLM() {
+        given().param("llm", true)
+            .when().get("/quarkus/load")
+            .then()
+            .statusCode(200)
+            .body(startsWith("Quarkus: load: 1 Cpu Iterations - 1 Memory Bites - DB false - LLM true"))
+            .body(containsString("The prompt has been received from the LLM"))
+            .body(endsWith("nano-seconds."));
+    }
+
+    @Test
+    void testStats() {
+        given()
+            .when().get("/quarkus/stats")
+            .then()
+            .statusCode(200);
+    }
 }
